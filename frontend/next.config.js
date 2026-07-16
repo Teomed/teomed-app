0960922c8bc623fafd8bd6b0d3af30589ee1185d
correalo <82238324+correalo@users.noreply.github.com>
2026-07-16 13:11:49 -0300

    Adiciona proxy para teomed-filemaker

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   distDir: process.env.NEXT_DIST_DIR || '.next',
+
+  async rewrites() {
+    return [
+      {
+        source: '/teomed-filemaker',
+        destination:
+          'https://frontend-production-d0898.up.railway.app/teomed-filemaker',
+      },
+      {
+        source: '/teomed-filemaker/:path*',
+        destination:
+          'https://frontend-production-d0898.up.railway.app/teomed-filemaker/:path*',
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
